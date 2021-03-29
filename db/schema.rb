0247b14b98cfaf41805409b8ecd91ca5a697c42a
Henry <vinsen1109@gmail.com>
2021-03-29 17:16:20 +0800

    add travis.yml

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2021_03_26_151220) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "task_id"
+    t.bigint "task_id"
     t.index ["task_id"], name: "index_tags_on_task_id"
   end
 
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_03_26_151220) do
     t.string "end_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "title"
     t.text "content"
     t.index ["user_id"], name: "index_tasks_on_user_id"
