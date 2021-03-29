af303de9a0428a00a2e17250bcf1c8c96f68f152
Henry <vinsen1109@gmail.com>
2021-03-29 14:13:25 +0800

    edit done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,39 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+
+    if @task.save
+      redirect_to root_path, notice: "Task Created"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+
+    if @task.update(task_params)
+      redirect_to root_path, notice: "Task Updated"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
