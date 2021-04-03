8432a3b121edbcf2d695ec95001dd8ebd012b26b
Henry <vinsen1109@gmail.com>
2021-04-03 19:16:13 +0800

    feat: task_model 使用 create_at 做為排序 docs: 增加 i18n 及 task_index 顯示 create_time test: 新增 create_time 排序測試

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   # belongs_to :user
   has_many :tags
+
+  default_scope -> { order('created_at DESC')}
 end
--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -42,6 +42,16 @@ RSpec.feature 'Delete Task Flow' do
   end
 end
 
+RSpec.feature 'index' do
+  let!(:first_task) { Task.create(title: 'first_task', content: 'first') }
+  let!(:new_task) { Task.create(title: 'new_task', content: 'new') }
+  scenario 'task desc' do
+    visit root_path
+
+    expect(page.body.index(new_task.title)).to be < page.body.index(first_task.title)
+  end
+end
+
 private
 
   def fill_data(title, content)
