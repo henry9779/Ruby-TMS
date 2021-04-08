dd47cac6c7a5821d3f540bee0d3e08acb2f38ed6
Henry <vinsen1109@gmail.com>
2021-04-08 15:17:36 +0800

    refactor: task_create 及 task_update 部分用 method 改寫

--- spec/features/task_update_spec.rb
+++ spec/features/task_update_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Update Task Flow' do
   let!(:first_task) { Task.create(title: 'hello', content: 'world') }
+
   scenario 'update task' do
     visit edit_task_path(first_task)
 
@@ -11,9 +12,10 @@ RSpec.feature 'Update Task Flow' do
     expect(page).to have_content 'Ruby'
     expect(page).to have_content 'Rails'
     expect(page).to have_content '2050-12-25 12:25:00 +0800'
-    expect(Task.first.title).to eq 'Ruby'
-    expect(Task.first.content).to eq 'Rails'
-    expect(Task.first.end_at).to eq '2050-12-25 12:25:00 +0800'
+    expect_task_data_eql(Task.first,
+                        title: 'Ruby',
+                        content: 'Rails',
+                        end_at: '2050-12-25 12:25:00 +0800')
     expect(current_path).to eq root_path
     expect(page).to have_content I18n.t('task.updated')
   end
