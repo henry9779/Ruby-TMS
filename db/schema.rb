102e2b1e9149e6c39c0ceca510eeda18a8aebccb
Henry <vinsen1109@gmail.com>
2021-04-20 16:53:08 +0800

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_121005) do
+ActiveRecord::Schema.define(version: 2021_04_20_084955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_121005) do
     t.text "content"
     t.datetime "end_at"
     t.integer "status", default: 0
+    t.integer "priority", default: 0
     t.index ["status"], name: "index_tasks_on_status"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
