e5e7b36927c9a8302504e7f66de1ff4b4fdd917d
Henry <vinsen1109@gmail.com>
2021-04-20 13:41:31 +0800

    fix: 修正 task_desc_spec 照建立時間排序錯誤

--- spec/features/task_desc_spec.rb
+++ spec/features/task_desc_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature 'Index' do
-  let!(:first_task) { Task.create(title: 'first_task', content: 'first') }
-  let!(:new_task) { Task.create(title: 'new_task', content: 'new') }
+  let!(:first_task) { Task.create(title: 'first task', content: 'first') }
+  let!(:second_task) { Task.create(title: 'second task', content: 'second') }
 
-  scenario 'task desc' do
+  scenario 'task sort by create time' do
     visit root_path
 
-    expect(page.body.index(new_task.title)).to be < page.body.index(first_task.title)
+    within(:xpath, '/html/body/table[2]/tr[1]') do
+      expect(page).to have_content('second task')
+    end
+
+    within(:xpath, '/html/body/table[2]/tr[2]') do
+      expect(page).to have_content('first task')
+    end
   end
 end
