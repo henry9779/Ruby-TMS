33a5d12aec7ad5005547db5cd2f70e709a3f06c5
Henry <vinsen1109@gmail.com>
2021-04-22 21:39:23 +0800

    feat: 任務標題或內容找不到時顯示無此資料，i18n test: 無此資料測試

--- spec/features/task_search_spec.rb
+++ spec/features/task_search_spec.rb
@@ -11,11 +11,27 @@ RSpec.feature 'Search Flow' do
     expect(page).to have_content 'hello'
   end
 
+  scenario 'search by title no found' do
+    visit root_path
+
+    fill_in 'q_title_cont', with: '...'
+    click_button I18n.t('button.search')
+    expect(page).to have_content I18n.t('no_record')
+  end
+
   scenario 'search by content' do
     visit root_path
 
     fill_in 'q_content_cont', with: 'world'
     click_button I18n.t('button.search')
     expect(page).to have_content 'world'
+  end
+
+  scenario 'search by content no found' do
+    visit root_path
+
+    fill_in 'q_content_cont', with: '...'
+    click_button I18n.t('button.search')
+    expect(page).to have_content I18n.t('no_record')
   end
 end
