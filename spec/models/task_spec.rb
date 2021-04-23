b17de7cf3f86e2b4780eb27b7b855bcb7587b32b
Henry <vinsen1109@gmail.com>
2021-04-23 16:43:38 +0800

    docs: 將 view / task priority 及 status 選項用 method 改寫 refactor: 將 model / task_spec 中 validation 及 search 用 describe 分開 chore: 將 spec / task_desc_by_priority_spec 使用 method 回傳結果

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,55 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  subject do
-    described_class.new(title: 'test title',
-                        content: 'test content',
-                        status: 'pending',
-                        end_at: (Date.today..))
+  describe 'validation' do
+    subject do
+      described_class.new(title: 'test title',
+                          content: 'test content',
+                          status: 'pending',
+                          end_at: (Date.today..))
+    end
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid whithout a title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a content' do
+      subject.content = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a status' do
+      subject.status = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is valid end_at can be nil' do
+      subject.end_at = nil
+      expect(subject).to be_valid
+    end
   end
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
+  describe 'search' do
+    subject do
+      described_class.new(title: 'test title',
+                          content: 'test content',
+                          status: 'pending',
+                          end_at: (Date.today..))
+    end
 
-  it 'is not valid whithout a title' do
-    subject.title = nil
-    expect(subject).to_not be_valid
-  end
+    it 'can search by title' do
+      subject.save
+      result = Task.ransack(title_cont: 'test').result
+      expect(result.first.title).to eq 'test title'
+    end
 
-  it 'is not valid without a content' do
-    subject.content = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a status' do
-    subject.status = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is valid end_at can be nil' do
-    subject.end_at = nil
-    expect(subject).to be_valid
-  end
-end
-
-RSpec.describe Task, type: :model do
-  subject do
-    described_class.new(title: 'test title',
-                        content: 'test content',
-                        status: 'pending',
-                        end_at: (Date.today..))
-  end
-
-  it 'can search by title' do
-    subject.save
-    result = Task.ransack(title_cont: 'test').result
-    expect(result.first.title).to eq 'test title'
-  end
-
-  it 'can search by content' do
-    subject.save
-    result = Task.ransack(content_cont: 'test').result
-    expect(result.first.content).to eq 'test content'
+    it 'can search by content' do
+      subject.save
+      result = Task.ransack(content_cont: 'test').result
+      expect(result.first.content).to eq 'test content'
+    end
   end
 end
