13b49298f3733f710f2904d00d28f7c312181227
Henry Huang <73519702+henry9779@users.noreply.github.com>
2021-05-27 20:33:52 +0800

    doc: 新增密碼加密及email格式驗證

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,23 @@
 class User < ApplicationRecord
   has_many :tasks
 
-  validates :name, presence: { message: I18n.t('name.blank') }
-  validates :email, presence: { message: I18n.t('email.blank') }, uniqueness: true
-  validates :password, presence: { message: I18n.t('password.blank') }, confirmation: true, length: { minimum: 8 }
+  validates :name,  presence: { message: I18n.t('name.blank') }
+
+  validates :email,
+            presence: { message: I18n.t('email.blank') },
+            format: { with: /[\w]+@([\w-]+\.)+[\w-]{2,4}/,
+            message: I18n.t('email.format') },
+            uniqueness: true
+
+  validates :password,
+            presence: { message: I18n.t('password.blank') },
+            confirmation: true,
+            length: { minimum: 8 }
+
+  before_create :encrypt_password
+
+  private
+  def encrypt_password
+    self.password = Digest::SHA1.hexdigest("r#{self.password}y")
+  end
 end
