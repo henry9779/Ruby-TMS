97cc7b1a9a5452bc893ee7554a046ef67d9ab9f3
Henry Huang <73519702+henry9779@users.noreply.github.com>
2021-05-27 19:49:07 +0800

    doc: add registrations controllers & view & i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'tasks#index'
 
-  resources :users
+  # resources :users
 
   resources :tasks, shallow: true
   resources :tags, shallow: true
+
+  get '/users/sign_up', to: 'registrations#new'
+  post '/users', to: 'registrations#create'
 
 end
