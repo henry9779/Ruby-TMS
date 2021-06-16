fd5f34277760745d748e73a90aadcc5c43d3e803
Henry Huang <73519702+henry9779@users.noreply.github.com>
2021-06-16 22:09:09 +0800

    feat: 註冊會員增加 recaptcha 功能

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
 # registrations_controller
 class RegistrationsController < ApplicationController
+  before_action :check_recaptcha_v2, only: [:create]
+
   def new
     @user = User.new
   end
@@ -21,5 +23,13 @@ class RegistrationsController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def check_recaptcha_v2
+    valid = verify_recaptcha secret_key: ENV['RECAPTCHA_V2_SECRET_KEY']
+
+    if not valid
+      redirect_to new_user_registration_path
+    end
   end
 end
